1f52ea7f8c0657068369d94bb5ec7f0add2ae75b
Enise <eniseaslan2001@gmail.com>
2024-08-23 12:59:01 +0300

    home ve about düzenlemeleri

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Routes,Route} from 'react-router-dom';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
